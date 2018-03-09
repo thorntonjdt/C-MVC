c7bb0f5d3fdcbc1bf150c2068a759ab5136e4460
Jake <thorntonjdt@gmail.com>
2018-03-09 15:22:31 -0800

    Add project files.

---
+++ MVCMovies/Migrations/20180309231159_Ratings.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace MVCMovies.Migrations
+{
+    public partial class Ratings : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Movie",
+                maxLength: 60,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Rating",
+                table: "Movie",
+                maxLength: 5,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Genre",
+                table: "Movie",
+                maxLength: 30,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Movie",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 60);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Rating",
+                table: "Movie",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 5);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Genre",
+                table: "Movie",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 30);
+        }
+    }
+}
